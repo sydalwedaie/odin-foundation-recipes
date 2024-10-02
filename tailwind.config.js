75114ccc049a90451d138687b143ffb9c0fa113d
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2024-10-02 18:00:51 +0300

    Initialize repository

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./index.js",
+    "./src/layouts/*.hbs",
+    "./src/components/*.hbs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
