bc85d28ce8786aba17a2d7fceadc4b17f1ee39ef
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2024-10-03 17:22:41 +0300

    Add recipe data

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,50 @@
 export const data = {
   title: 'Odin Recipes',
+  recipes: [
+    {
+      name: 'Pan-Roasted Chicken Breasts',
+      imgURL: '/src/assets/img/pan-roasted-chicken-breasts.webp',
+      ingredients: [
+        '4 boneless chicken breast halves with skin',
+        'salt and freshly ground black pepper to taste',
+        '2 tablespoons olive oil',
+        '1 tablespoon chopped fresh herbs (thyme, parsley, rosemary) (Optional)',
+        '¼ cup apple cider vinegar',
+        '4 tablespoons cold butter, cut into small pieces',
+        '1 tablespoon chicken broth (or water), if needed to thin sauce',
+      ],
+      step: [
+        'Gather all ingredients.',
+        'Season chicken on both sides with salt and pepper.',
+        'Heat olive oil in a heavy skillet over medium-high heat until it starts to shimmer. Place chicken breasts skin-side down in the skillet. ',
+        'Sprinkle with fresh herbs. Cook until the bottoms are well seared and golden brown, 5 to 6 minutes.',
+        'Flip chicken breasts over and continue cooking until an instant-read thermometer inserted into the center of a breast reads 150 degrees F (66 degrees C), about 5 minutes.',
+        'Add vinegar and cold butter to the pan with chicken. Shake the pan gently until butter melts and the internal temperature of chicken reads at least 165 degrees F (74 degrees C), 2 to 3 minutes more.',
+        'Stir in 1 tablespoon chicken broth to thin sauce.',
+      ],
+    },
+    {
+      name: 'Chicken Berry Salad',
+      imgURL: '/src/assets/img/chicken-berry-salad.webp',
+      ingredients: [
+        '1 (.75 ounce) packet honey mustard salad dressing mix',
+        '¼ cup cider vinegar',
+        '½ cup vegetable oil',
+        '2 tablespoons orange juice',
+        '1 pound skinless, boneless chicken breast halves',
+        '8 cups mixed salad greens',
+        '1 cup sliced fresh strawberries',
+        '½ cup fresh blueberries',
+        '½ cup fresh raspberries',
+        '8 ounces sugar snap peas',
+        '½ cup toasted pecans',
+      ],
+      step: [
+        'In a medium bowl, prepare the dressing according to package directions, using vinegar and oil, and substituting orange juice for the water; set aside.',
+        'Preheat the grill for high heat.',
+        'Lightly oil the grill grate. Grill the chicken 6 to 8 minutes on each side, or until juices run clear. Remove from heat, cool, and slice into strips.',
+        'In a large bowl, toss together the chicken, salad greens, strawberries, blueberries, raspberries, peas, and pecans. Pour in the prepared dressing, and toss to coat.',
+      ],
+    },
+  ],
 };
