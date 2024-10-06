9fd8b87f39f45d3cba9248418e7ccc111f260cfd
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2024-10-06 13:27:23 +0300

    Finishig touches

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Odin Recipes
+
+The solution to the [Odin Recipes](https://www.theodinproject.com/lessons/foundations-recipes) project from The Odin Project. Configured using Vite, Tailwind CSS, and Handlebars.
+
+## Installation
+
+To install the dependencies and build the project, run the following command in the root of the project:
+
+```bash
+git clone git@github.com:sydalwedaie/odin-foundation-recipes.git
+cd odin-foundations-recipes
+npm install
+npm run dev
+```
+
+## What I learned
+
+I may have gone a bit too far with my Zenful Template. The whole purpose of the template was to simplify my practice project routine. However, I felt like I was making my own framework. My main issue came up trying to build the other pages for this website; namely the recipe pages. I had to modify the Vite config file so that those pages would show up in production. It was too complicated, but I still gained a valuable experience.
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,32 @@
+import { defineConfig } from "vite";
 import { resolve } from "path";
 import { data } from "./src/data/index.js";
 import handlebars from "vite-plugin-handlebars";
 
-export default {
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Specify individual HTML files here
+        main: resolve(__dirname, "/index.html"),
+        recipe1: resolve(__dirname, "src/pages/chicken-berry-salad.html"),
+        recipe2: resolve(
+          __dirname,
+          "src/pages/pan-roasted-chicken-breasts.html",
+        ),
+        recipe3: resolve(
+          __dirname,
+          "src/pages/penne-with-chicken-and-asparagus.html",
+        ),
+      },
+      output: {
+        // Make sure the HTML files end up in the root of the dist folder
+        entryFileNames: "[name].html",
+        // Disable chunking, so no extra JavaScript chunks are generated
+        manualChunks: undefined,
+      },
+    },
+  },
   plugins: [
     handlebars({
       partialDirectory: [
@@ -24,4 +48,4 @@ export default {
       },
     }),
   ],
-};
+});
